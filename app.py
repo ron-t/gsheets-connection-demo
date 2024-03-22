bfe3ae04356e3509399f9b4ef57c07ec74132340
ron-t <17290359+ron-t@users.noreply.github.com>
2024-03-22 13:36:13 +1300

    show as dataframe; parameterise sheet dimensions

--- app.py
+++ app.py
@@ -2,19 +2,29 @@ import streamlit as st
 import math
 from streamlit_gsheets import GSheetsConnection
 
+MAX_ROWS = 20
+MAX_COLS = 3
+
+with st.form("form"):
+    num_rows = st.slider("How many rows?", 1, MAX_ROWS)
+    num_cols = st.slider("How many columns?", 1, MAX_COLS)
+
+    submitted = st.form_submit_button("Retrieve")       
+
 # credit: https://docs.streamlit.io/knowledge-base/tutorials/databases/private-gsheet
 # Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection, ttl=5.0)
+if submitted:
+    conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Sheet1",
-    ttl="10m",
-    usecols=[0, 1],
-    nrows=20,
-)
+    df = conn.read(
+        worksheet="Sheet1",
+        ttl="5s",
+        usecols=range(0,num_cols),
+        nrows=num_rows,
+    )
 
-df = df.dropna(thresh=1)
+    df = df.dropna(thresh=1)
 
-# Print results.
-for row in df.itertuples():
-        st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"First {num_rows} rows and {num_cols} columns from {conn.client._spreadsheet}")
+    # Print results.
+    st.dataframe(df, hide_index=True)
